9e9326d08a5753e00884ffa35b26539a74a718f7
Wolfgang Preinfalk <w.preinfalk@outlook.de>
2024-02-04 18:13:41 +0100

    Update Error1.jsx

--- src/components/error/Error1.jsx
+++ src/components/error/Error1.jsx
@@ -17,17 +17,14 @@ const Error1 = () => {
                   src={Thumb404}
                   alt="Page not found"
                 />
-                <h2>Sorry! page did not found</h2>
+                <h2>Sorry! Page not found</h2>
                 <p>
                   {
                     "The page you are looking for doesn't exist or has been moved"
                   }
                 </p>
                 <div className="btn_wrapper">
-                  <Link
-                    href="/digital-marketing"
-                    className="wc-btn-primary btn-hover btn-item"
-                  >
+                  <Link href="/" className="wc-btn-primary btn-hover btn-item">
                     <span></span> Back to <br />
                     Homepage <i className="fa-solid fa-arrow-right"></i>
                   </Link>
